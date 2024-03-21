bdcd02137cfa164d7c9fa4fa843a48d12f206930
sharans17 <79459918+sharans17@users.noreply.github.com>
2024-03-21 10:02:40 +0530

    Update api_tokens.py

--- api_tokens.py
+++ api_tokens.py
@@ -1,5 +1,5 @@
 import os
 
-os.environ["PINECONE_API_KEY"] = "756b39ac-df52-4532-b93c-06b3b409e08e"
-os.environ["LLAMA_API_KEY"] = "LL-VB7pFE92RdBRfvuLoTgF0ZiTd1gNlhLLUWKfhwUQ9UePrDALWdq9ee91UbOywdyy"
-os.environ["COHERE_API_KEY"]= "zcHSs3J3KLSfen2ouWcihWJHNlVgL2fvdfpENCkt"
+os.environ["PINECONE_API_KEY"] = "YOURPINECONEAPIKEY"
+os.environ["LLAMA_API_KEY"] = "YOURLLAMAAPIKEY"
+os.environ["COHERE_API_KEY"]= "YOURCOHEREAPIKEY"
